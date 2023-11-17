492cb8551aaaff15605766ac013a04a7a796e973
TC1005 <curvafans383@gmail.com>
2023-11-17 14:17:05 +0700

    Menambahkan Halaman dan format

---
+++ app/src/main/java/com/example/pam2/Komponen/Format.kt
@@ -0,0 +1,2 @@
+package com.example.pam2.Komponen
+
--- app/src/main/java/com/example/pam2/MainActivity.kt
+++ app/src/main/java/com/example/pam2/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam2.ui.theme.HalamanHome
 import com.example.pam2.ui.theme.PAM2Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PAM2Theme {
-        Greeting("Android")
+        HalamanHome(onNextButtonClicked = {})
     }
 }
